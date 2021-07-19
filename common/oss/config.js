3c20aa9c5c257e90605c4ce88ec4bfa42aac3563
JiangHaoLiuGit <55119450+JiangHaoLiuGit@users.noreply.github.com>
2021-07-19 17:00:07 +0800

    Update config.js

--- common/oss/config.js
+++ common/oss/config.js
@@ -1,17 +1,10 @@
-var fileHost = "https://liujianghao.oss-cn-hangzhou.aliyuncs.com"
-//刘江浩
-//https://liujianghao.oss-cn-hangzhou.aliyuncs.com
-//LTAI4GDGEKa3eWzE6cXfHxfJ
-//LMzo6CAw8KAJsPwBh352Y1DQOh3obj
-//松格科技
-//https://songgete.oss-accelerate.aliyuncs.com
-//LTAI4GAADVDQTFKcA5zNW9dd
-//1kKqFmVr3pT4lvTxS8gmsYhpPb6bfU
+var fileHost = ""
+
 var config = {
   //aliyun OSS config
   uploadImageUrl: `${fileHost}`, //默认存在根目录，可根据需求改
-  OSSAccessKeyId: 'LTAI4GDGEKa3eWzE6cXfHxfJ',
-  AccessKeySecret: 'LMzo6CAw8KAJsPwBh352Y1DQOh3obj',
+  OSSAccessKeyId: '',
+  AccessKeySecret: '',
   timeout: 87600 //这个是上传文件时Policy的失效时间
 };
 module.exports = config
