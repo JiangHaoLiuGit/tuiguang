76efec2d8f2bf58341ab02b50ccc72b9078b41bc
JiangHaoLiuGit <55119450+JiangHaoLiuGit@users.noreply.github.com>
2022-04-07 15:20:57 +0800

    Update config.js

--- common/oss/config.js
+++ common/oss/config.js
@@ -2,9 +2,7 @@ var fileHost = ""
 
 var config = {
   //aliyun OSS config
-  uploadImageUrl: `${fileHost}`, //默认存在根目录，可根据需求改
-  OSSAccessKeyId: '',
-  AccessKeySecret: '',
+  
   timeout: 87600 //这个是上传文件时Policy的失效时间
 };
 module.exports = config
